389ce93dd09df206590e9568c253d8d14c7a9cd9
Jonathan Hewer <jonhewer@gmail.com>
2016-03-16 22:52:33 +0000

    Add a test which checks that help information is printed when no command is supplied.

--- test/tests/cli.js
+++ test/tests/cli.js
@@ -6,6 +6,7 @@ describe("cli", function() {
     //system under test
     const sut = require("../../lib/cli");
 
+    const commander = require("commander");
     const generate = require("../../lib/generate");
     const log = require("../../lib/log");
     const init = require("../../lib/init");
@@ -14,17 +15,27 @@ describe("cli", function() {
 
     //stubs
     let generateCommand;
+    let helpCommand;
     let increaseLogVerbosity;
     let initCommand;
 
     beforeEach(function () {
         generateCommand = sandbox.stub(generate, "generate");
+        helpCommand = sandbox.stub(commander, "help");
         increaseLogVerbosity = sandbox.stub(log, "increaseVerbosity");
         initCommand = sandbox.stub(init, "init");
     });
 
     afterEach(function () {
         sandbox.restore();
+    });
+
+    it("should print help if no command is supplied", function () {
+        process.argv = [ "", "" ];
+
+        sut();
+
+        helpCommand.should.be.calledOnce;
     });
 
     describe("generate command", function () {
